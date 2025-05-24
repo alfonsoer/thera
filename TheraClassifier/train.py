17bdfd91ebf83889c1fe3873f601386779ed6c38
alfonsoer <32367293+alfonsoer@users.noreply.github.com>
2025-05-24 08:12:43 +0200

    Added scheduler and comments

--- TheraClassifier/train.py
+++ TheraClassifier/train.py
@@ -12,14 +12,15 @@ import torch.optim as optim
 from sklearn.model_selection import StratifiedKFold
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from model import SimpleCNN
+from model import SimpleCNN, 
 from dataset import TrainDataset
 from utils import compute_metrics, plot_metrics
 import os
 import copy as cp
 import pandas as pd
 
-def train_model(image_dir, labels_csv, epochs=10, lr=0.001,save_dir=''):
+def train_model(image_dir, labels_csv, epochs=10, lr=0.001,
+                step_size=5, gamma=0.7, save_dir='', cnn_type='basic'):
     transform = transforms.Compose([
         transforms.Resize((64, 64)),
         transforms.ToTensor()
@@ -36,23 +37,28 @@ def train_model(image_dir, labels_csv, epochs=10, lr=0.001,save_dir=''):
     os.makedirs(log_dir, exist_ok=True)
     os.makedirs(model_dir, exist_ok=True)
     
-    #Check cuda
-    num_workers=2
+    #Check GPU disponibility
+    move_to_gpu = False
+    if torch.cuda.is_available():
+        move_to_gpu=True    
+                    
+    #Useful when having mutiple GPUs
     device_id = 0     
     cuda_list= [ 'cuda:0',
                  'cuda:1',
                  'cuda:2',
                  'cuda:3'
             ]
+    #Number of threads for the dataloader to read data simultaneously 
+    num_workers=2
     
     #Read the dataset
     dataset     = TrainDataset(image_dir, labels_csv, transform=transform, 
                                train_transform=None, stage='')
+    labels = [label for _, label in dataset]
 
     #Dataset splitting into K=5 stratified folds
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-
-    labels = [label for _, label in dataset]
 
     for fold, (train_idx, val_idx) in enumerate(skf.split(dataset, labels)):
         print(f"Fold {fold}")
@@ -60,7 +66,8 @@ def train_model(image_dir, labels_csv, epochs=10, lr=0.001,save_dir=''):
         tr_subset  = cp.copy(dataset)
         val_subset = cp.copy(dataset)
         tr_subset.subset(train_idx) 
-        val_subset.subset(val_idx) 
+        val_subset.subset(val_idx)
+        
         #Append extra tranformations to training data
         tr_subset.appendTrainTransform(train_transform)
         
@@ -68,31 +75,39 @@ def train_model(image_dir, labels_csv, epochs=10, lr=0.001,save_dir=''):
                                   shuffle=True, num_workers=num_workers)
         val_loader = DataLoader(val_subset, batch_size=32,
                                 num_workers=num_workers)
-        #Model setup
+        #Model setup. 
         model = SimpleCNN()
-        model = model.cuda() if torch.cuda.is_available() else model
+        #Verify GPU availability. This is also done for the inputs as well as for the labels
+        model = model.cuda() if move_to_gpu else model
         #Using Adam optimizer
-        optimizer = optim.Adam(model.parameters(), lr=lr)
+        optimizer = optim.Adam(model.parameters(), lr=lr)s
         #And a binary cross entropy loss function to minimize
         criterion = nn.BCEWithLogitsLoss()
+        #I will set an scheduler to control the learning rate
+        scheduler = None
+        if scheduler_str == 'StepLR':
+            scheduler = StepLR(optimizer, step_size=step_size, gamma=gamma) 
         history = {'train_metrics': [],  'val_metrics': []}
         for epoch in range(epochs):
-            print(f"  Epoch {epoch + 1}/{epochs}")
+            lr_start = optimizer.state_dict()['param_groups'][0]['lr']
+            print(f"  Epoch {epoch + 1}/{epochs}, lr={lr_start}")
+            #Model mode as training
             model.train()
             train_labels = []
             train_probs = []
             epoch_loss = 0.0
             for inputs, labels in train_loader:
-                inputs = inputs.cuda() if torch.cuda.is_available() else inputs
-                labels = labels.float().unsqueeze(1).cuda() if torch.cuda.is_available() else labels.float().unsqueeze(1)
-                optimizer.zero_grad()
+                inputs = inputs.cuda() if move_to_gpu else inputs
+                labels = labels.float().unsqueeze(1).cuda() if move_to_gpu else labels.float().unsqueeze(1)
+                optimizer.zero_grad() #Gradients init before each evaluation
                 outputs = model(inputs)
                 loss = criterion(outputs, labels)
-                loss.backward()
+                loss.backward() #Loss backpropagation
                 optimizer.step()
                 epoch_loss+= loss.item() * inputs.size(0)
                 
-                with torch.no_grad():
+                with torch.no_grad():#Reduce memory use by disabling gradient calculation
+                    #.cpu transfers data from GPU to the CPU if is the case
                     probs = torch.sigmoid(outputs).squeeze().cpu().numpy()
                     train_probs.extend(probs.tolist())
                     train_labels.extend(labels.squeeze().cpu().numpy().tolist())
@@ -110,10 +125,10 @@ def train_model(image_dir, labels_csv, epochs=10, lr=0.001,save_dir=''):
             all_labels = []
             all_probs = []
             epoch_loss = 0.0
-            with torch.no_grad():
+            with torch.no_grad(): #Reduce memory use by disabling gradient calculation
                 for inputs, labels in val_loader:
-                    inputs = inputs.cuda() if torch.cuda.is_available() else inputs
-                    labels = labels.cuda() if torch.cuda.is_available() else labels
+                    inputs = inputs.cuda() if move_to_gpu else inputs
+                    labels = labels.cuda() if move_to_gpu else labels
                     outputs = model(inputs)
                     probs = torch.sigmoid(outputs).squeeze().cpu().numpy()
                     all_probs.extend(probs.tolist())
@@ -122,10 +137,11 @@ def train_model(image_dir, labels_csv, epochs=10, lr=0.001,save_dir=''):
             epoch_val_loss = epoch_loss / len(val_loader.dataset)
             val_metrics = compute_metrics(all_labels, all_probs)
             val_metrics['loss'] =  epoch_val_loss
-            history['val_metrics'].append(val_metrics) 
+            history['val_metrics'].append(val_metrics)
+            scheduler.step() #Step to next learning rate change
             print(f"    Val   -> Loss: {val_metrics['loss']:.4f}, HTER: {val_metrics['HTER']:.4f}, FAR: {val_metrics['FAR']:.4f}, FRR: {val_metrics['FRR']:.4f}, "
                   f"Balanced Acc: {val_metrics['Balanced Accuracy']:.4f}, AUC: {val_metrics['AUC']:.4f}")
-
+            
         torch.save(model.state_dict(),  os.path.join(model_dir,'fold_'+str(fold)+'.pt'))
         print(f"Model saved for fold {fold}")
 
